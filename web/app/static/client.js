9e14d9f971f642fa415a64b58f766bc621e5e42f
tank671 <43883333+tank671@users.noreply.github.com>
2019-01-20 20:13:09 +0000

    new folder org, not working yet though

---
+++ web/app/static/client.js
@@ -0,0 +1,44 @@
+var el = x => document.getElementById(x);
+var textData = "";
+
+function showPicker(inputId) { el('file-input').click(); }
+
+function showPicked(input) {
+    el('upload-label').innerHTML = input.files[0].name;
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        el('image-picked').src = e.target.result;
+        el('image-picked').className = '';
+    }
+    reader.readAsDataURL(input.files[0]);
+}
+
+function showText(input) {
+    textData = String(input.value);
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        el('text-input').src = e.target.result;
+        el('text-input').className = '';
+    }
+}
+
+function analyze() {
+
+    el('analyze-button').innerHTML = 'Generating...';
+    el('result-label').innerHTML = 'Please refresh and try again if nothing happens in a few minutes.'
+    var xhr = new XMLHttpRequest();
+    var loc = window.location
+    xhr.open('POST', `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`, true);
+    xhr.onerror = function() {alert (xhr.responseText);}
+    xhr.onload = function(e) {
+        if (this.readyState === 4) {
+            var response = JSON.parse(e.target.responseText);
+            el('result-label').innerHTML = `${response['result']}`;
+        }
+        el('analyze-button').innerHTML = 'Generate';
+    }
+
+    var fileData = new FormData();
+    fileData.append('file', textData);
+    xhr.send(fileData);
+}
